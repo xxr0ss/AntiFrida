da9fd87d20f50a1ff2b9b1b754a8e646463daef8
Jiaxin Peng <pjx206@163.com>
2022-03-26 14:29:37 +0800

    Remove unused code

--- app/src/main/java/com/xxr0ss/antifrida/utils/AntiFridaUtil.kt
+++ app/src/main/java/com/xxr0ss/antifrida/utils/AntiFridaUtil.kt
@@ -1,10 +1,9 @@
 package com.xxr0ss.antifrida.utils
 
-import android.app.ActivityManager
 import java.io.File
 import java.lang.Exception
 import android.util.Log
-import java.net.Socket
+
 
 object AntiFridaUtil {
     private var TAG: String = "AntiFridaUtil"
@@ -13,21 +12,10 @@ object AntiFridaUtil {
     fun checkFridaByModuleEnum(target: String): Boolean {
         val maps = getProcMaps()
         Log.d(TAG, maps)
-        return maps.contains(target)
+        return target in maps
     }
 
     external fun checkFridaByPort(port: Int): Boolean
-
-//    fun checkFridaByPort(port: Int): Boolean {
-//        try{
-//            val socket = Socket("127.0.0.1", port)
-//            socket.close()
-//            return true;
-//        }catch(e: Exception) {
-//            Log.e(TAG, e.stackTraceToString())
-//        }
-//        return false;
-//    }
 
 
     private fun getProcMaps(): String {
@@ -40,9 +28,6 @@ object AntiFridaUtil {
         return ""
     }
 
-    fun checkFridaByEnumRunningProcesses(): Boolean {
-        return false
-    }
 
     init {
         System.loadLibrary("antifrida")
