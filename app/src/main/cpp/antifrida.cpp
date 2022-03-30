b087ddc91f96b074aac3ecda9703c10f4b36d7a8
Jiaxin Peng <pjx206@163.com>
2022-03-30 21:39:40 +0800

    Added x86_64 support for testMyRead()

--- app/src/main/cpp/antifrida.cpp
+++ app/src/main/cpp/antifrida.cpp
@@ -8,6 +8,8 @@
 #include <fcntl.h>
 #include <android/log.h>
 
+#define unused_param(x) (x)
+
 
 extern "C" int my_read(int, void *, int);
 
@@ -15,6 +17,7 @@ extern "C"
 JNIEXPORT jboolean JNICALL
 Java_com_xxr0ss_antifrida_utils_AntiFridaUtil_checkFridaByPort(JNIEnv *env, jobject thiz,
                                                                jint port) {
+    unused_param(thiz);
     struct sockaddr_in sa{};
     sa.sin_family = AF_INET;
     sa.sin_port = htons(port);
@@ -31,15 +34,18 @@ Java_com_xxr0ss_antifrida_utils_AntiFridaUtil_checkFridaByPort(JNIEnv *env, jobj
 }
 
 
+const int BUF_SIZE = 128;
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_xxr0ss_antifrida_utils_AntiFridaUtil_testMyRead(JNIEnv *env, jobject thiz) {
-    char buf[32];
+    unused_param(thiz);
+    char buf[BUF_SIZE];
     int fd;
     if ((fd = openat(AT_FDCWD, "/proc/self/maps", O_RDONLY, 0)) > 0) {
-        int size = my_read(fd, buf, 31);
+        int size = my_read(fd, buf, BUF_SIZE - 1);
         if (size == 0) {
-            __android_log_print(ANDROID_LOG_INFO, "JNI", "read 0 bytes", buf);
+            __android_log_print(ANDROID_LOG_INFO, "JNI", "read 0 bytes");
         } else {
             buf[size] = 0;
             __android_log_print(ANDROID_LOG_INFO, "JNI", "data %d: %s", size, buf);
